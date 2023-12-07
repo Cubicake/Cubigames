de8497a8a0356b5866d81b06f1c79bb70f6936a9
Cubicake <70577278+Cubicake@users.noreply.github.com>
2023-12-07 16:00:47 +1030

    Update news.js

--- news.js
+++ news.js
@@ -5,10 +5,16 @@ document.addEventListener('DOMContentLoaded', async function () {
     const response = await fetch('news-articles/article-list.json');
     const articleLocations = await response.json();
 
+    console.log("Article Locations:", articleLocations); // Log the fetched article locations
+
     // Load and display each news article
     for (const articleLocation of articleLocations) {
+        console.log("Fetching Article:", articleLocation); // Log the current article being fetched
+
         const articleResponse = await fetch(articleLocation);
         const article = await articleResponse.json();
+
+        console.log("Fetched Article Data:", article); // Log the fetched article data
 
         const articleDiv = document.createElement('div');
         articleDiv.id = article.id; // Set the id attribute for the article div
